24ab559bb8d4e44baa4218c8cf6ed589a468072e
kuzme <kuzmenkoserhii4@gmail.com>
2023-05-13 00:00:02 -0700

    123

---
+++ src/C1/pr1.java
@@ -0,0 +1,12 @@
+package C1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class pr1 {
+    public static void main(String[] args) {
+        System.out.println("LOL");
+
+        System.out.println("New Remote Repos");
+    }
+
+}
---
+++ src/C2/pr2.java
@@ -0,0 +1,7 @@
+package C2;
+
+public class pr2 {
+    public static void main(String[] args) {
+        System.out.println("Kek");
+    }
+}
